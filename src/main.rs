dae2e1f752cbcdf307f74b4e312c96c4b9a0691e
BoudBoud47 <mehdi.boudart@gmail.com>
2021-03-04 21:34:53 +0100

    Print matrix

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 
 
+pub fn print_matrix(matrix : [[i32; 5]; 5])  {
+    for x in &matrix {
+        let mut display_row  = " ".to_owned();
+        for y in x {
+            display_row.push_str(y.to_string().as_ref());
+            display_row.push_str(" ");
+        }
+        println!("{}", display_row);gio:wq:qqqq
+    }
 
-
-
-
+}
 
 
 fn main() {
-    println!("Hello, world!");
+    let matrix : [[i32; 5]; 5] = [
+        [0, 1, 1, 0, 0],
+        [1, 1, 0, 0, 0],
+        [1, 0, 0, 1, 0],
+        [1, 1, 0, 1, 1],
+        [0, 1, 0, 0, 0],
+    ];
+    print_matrix(matrix);
 }
